bff02b5d3e485a9395b80dd67da27418b18ea2c8
Nikita Tolstykh <nikitatolstyh7@gmail.com>
2025-12-16 19:15:02 +0100

    feat:cache name and cache assets in sw.js

---
+++ sw.js
@@ -0,0 +1,15 @@
+const CACHE_NAME = 'parking-reports-v1';
+const CACHE_ASSETS = [
+    './',
+    './index.html',
+    './create.html',
+    './reports.html',
+    './css/styles.css',
+    './js/app.js',
+    './js/camera.js',
+    './js/db.js',
+    './js/notifications.js',
+    './manifest.json',
+    './icons/icon-192.png',
+    './icons/icon-512.png'
+];
