301db5e5022d2aeb3fbf1a6c8cd9dad34dfd81a6
Nikita Tolstykh <nikitatolstyh7@gmail.com>
2025-12-16 19:16:00 +0100

    feat:Install Service Worker and cache assets

--- sw.js
+++ sw.js
@@ -13,3 +13,19 @@ const CACHE_ASSETS = [
     './icons/icon-192.png',
     './icons/icon-512.png'
 ];
+
+self.addEventListener('install', (event) => {
+    console.log('Service Worker: Installing...');
+
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Service Worker: Caching files');
+                return cache.addAll(CACHE_ASSETS).catch(err => {
+                    console.log('Service Worker: Some files failed to cache', err);
+                });
+            })
+            .then(() => self.skipWaiting())
+            .catch(err => console.log('Service Worker: Cache failed', err))
+    );
+});
