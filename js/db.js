56e814988ac55b1f27fa203d8454b334ad535e6b
Nikita Tolstykh <nikitatolstyh7@gmail.com>
2025-12-17 19:08:07 +0100

    feat:save report to IndexedDB

--- js/db.js
+++ js/db.js
@@ -30,3 +30,32 @@ function openDB() {
         };
     });
 }
+
+// Save report to IndexedDB
+async function saveReport(reportData) {
+    try {
+        const db = await openDB();
+
+        return new Promise((resolve, reject) => {
+            const transaction = db.transaction([STORE_NAME], 'readwrite');
+            const objectStore = transaction.objectStore(STORE_NAME);
+
+            const request = objectStore.add(reportData);
+
+            request.onsuccess = () => {
+                resolve(request.result);
+            };
+
+            request.onerror = () => {
+                reject('Error saving report');
+            };
+
+            transaction.oncomplete = () => {
+                db.close();
+            };
+        });
+    } catch (error) {
+        console.error('Error in saveReport:', error);
+        throw error;
+    }
+}
